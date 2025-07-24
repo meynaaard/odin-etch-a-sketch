91927f636b8860eed8e59f48afb5537b010b813d
meynard <sombritomeynard@gmail.com>
2025-07-24 10:17:31 +0800

    feat: Add hover effect for each grid cell
    This change adds an event listeners for each grid
    cell to add a '.hovered' class that changes the
    background color upon `mouseenter`, and then
    removes event handlers upon `mouseleave`.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,26 @@ for (let i = 0; i < 16; i++) {
     gridSquare.classList.add('grid-square');
   }
 }
+
+const gridSquares = document.querySelectorAll('.grid-container .grid-square');
+gridSquares.forEach(gridSquare => {
+  gridSquare.addEventListener('mouseenter', draw);
+});
+gridSquares.forEach(gridSquare => {
+  gridSquare.addEventListener('mouseleave', removeEventHandler);
+});
+
+function draw(event) {
+  const target = event.target;
+
+  if (!target.classList.contains('hovered')) {
+    target.classList.add('hovered');
+  }
+}
+
+function removeEventHandler(event) {
+  const target = event.target;
+
+  target.removeEventListener('mouseenter', draw);
+  target.removeEventListener('mouseleave', removeEventHandler);
+}
