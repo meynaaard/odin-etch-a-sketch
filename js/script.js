1ca722e6d76d00bb4aea7f134a80d04a7e144f39
meynard <sombritomeynard@gmail.com>
2025-07-24 13:13:28 +0800

    feat: Add button that can change size of grid
    This change adds a button that can change the
    sketch pad grid size. The grid size should be
    greater than 0 and less than 100

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
 const gridContainer = document.querySelector('.grid-container');
+const sizeButton = document.querySelector('.size-button');
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0;  j < 16; j++) {
-    const gridSquare = document.createElement('div');
-    gridContainer.appendChild(gridSquare);
-    gridSquare.classList.add('grid-square');
+createGrid(16); // Create initial 16x16 grid
+
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const gridSquare = document.createElement('div');
+      gridContainer.appendChild(gridSquare);
+      gridSquare.classList.add('grid-square');
+      gridSquare.style.width = `calc(100% / ${gridSize})`;
+    }
   }
-}
+  
+  const gridSquares = gridContainer.querySelectorAll('.grid-square');
 
-const gridSquares = document.querySelectorAll('.grid-container .grid-square');
-gridSquares.forEach(gridSquare => {
-  gridSquare.addEventListener('mouseenter', draw);
-});
-gridSquares.forEach(gridSquare => {
-  gridSquare.addEventListener('mouseleave', removeEventHandler);
-});
+  gridSquares.forEach(gridSquare => {
+    gridSquare.addEventListener('mouseenter', draw);
+  });
+
+  gridSquares.forEach(gridSquare => {
+    gridSquare.addEventListener('mouseleave', removeEventHandler);
+  });
+}
 
 function draw(event) {
   const target = event.target;
@@ -30,3 +38,16 @@ function removeEventHandler(event) {
   target.removeEventListener('mouseenter', draw);
   target.removeEventListener('mouseleave', removeEventHandler);
 }
+
+sizeButton.addEventListener('click', () => {
+  const gridSize = parseInt(prompt('Change sketchpad size: (100 max)', 0));
+  const gridSquares = gridContainer.querySelectorAll('.grid-square');
+
+  if (gridSize > 0 && gridSize <= 100) {
+    gridSquares.forEach(gridSquare => {
+      gridSquare.remove();
+    });
+
+    createGrid(gridSize);
+  }
+});
